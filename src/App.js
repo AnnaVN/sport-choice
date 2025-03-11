50eae20bf9fb03c8cc0d6a8d3ac07cb9ed7d0a71
Ana <nagdaseva.ana@gmail.com>
2025-03-11 10:23:32 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const firstName = prompt("What is your name?");
+  return (
+    <>
+      <h1 className="big">Hey, {firstName}! Nice to meet you!</h1>
+      <Sport />
+    </>
+  );
+}
+
+const Sport = () => {
+  return (
+    <div>
+      <h1 className="big">What kind of sport is your favourite?</h1>
+      <input
+        class="btn"
+        type="button"
+        value="swim"
+        onClick={() => {
+          alert("Nice shoice!");
+        }}
+      />
+      <br />
+
+      <input
+        class="btn"
+        type="button"
+        value="soccer"
+        onClick={() => {
+          alert("Nice shoice!");
+        }}
+      />
+
+      <br />
+      <input
+        class="btn"
+        type="button"
+        value="backetball"
+        onClick={() => {
+          alert("Nice shoice!");
+        }}
+      />
+      <br />
+      <input
+        class="btn"
+        type="button"
+        value="marshal_arts"
+        onClick={() => {
+          alert("Nice shoice!");
+        }}
+      />
+    </div>
+  );
+};
+
+export default App;
